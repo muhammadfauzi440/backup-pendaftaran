e51f028f9fc7f980ab5274626362a504cbd94926
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-03-02 08:00:33 +0700

    ubah ukruan  text

--- resources/views/admin/instansi/index.blade.php
+++ resources/views/admin/instansi/index.blade.php
@@ -4,7 +4,7 @@
     <div class="max-w-6xl mx-auto">
         <div class="flex justify-between items-end mb-10">
             <div>
-                <h1 class="text-4xl font-black text-gray-900 uppercase tracking-tighter">Kelola Instansi</h1>
+                <h1 class="text-2xl font-black text-gray-900 uppercase tracking-tighter">Kelola Instansi</h1>
                 <p class="text-gray-500 font-bold mb-4">Tambah atau hapus daftar Instansi</p>
             </div>
         </div>
