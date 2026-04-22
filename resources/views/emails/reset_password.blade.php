73c4060c491ef5888d5f70fcd7ab1d6ea6c0776e
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-04-22 14:12:56 +0700

    fix: koreksi nama PT GI

--- resources/views/emails/reset_password.blade.php
+++ resources/views/emails/reset_password.blade.php
@@ -9,10 +9,10 @@
     <div style="background-color: white; max-width: 600px; margin: auto; padding: 40px; border-radius: 20px;">
         <h2 style="text-transform: uppercase; letter-spacing: -1px;">Permintaan Atur Ulang Sandi</h2>
         <p>Halo,</p>
-        <p>Kami menerima permintaan untuk mengatur ulang kata sandi akun Anda di <strong>Portal Magang PT GI</strong>. Silakan klik tombol di bawah ini untuk melanjutkan:</p>
+        <p>Kami menerima permintaan untuk mengatur ulang kata sandi akun Anda di <strong>Portal Magang PT Global Intermedia Nusantara</strong>. Silakan klik tombol di bawah ini untuk melanjutkan:</p>
         
         <div style="text-align: center; margin: 40px 0;">
-            <a href="{{ route('password.reset', ['token' => $token, 'email' => $email]) }}" class="btn">RESET KATA SANDI</a>
+            <a href="{{ route('password.reset', ['token' => $token, 'email' => $email]) }}" class="btn" style="color: white;">RESET KATA SANDI</a>
         </div>
 
         <p style="font-size: 12px; color: #777;">Tautan ini akan kedaluwarsa dalam 60 menit. Jika Anda tidak merasa meminta hal ini, abaikan saja email ini.</p>
