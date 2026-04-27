19ceaf9a15fcfe9da9c8cc8700386d434f523a5e
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-04-27 07:57:39 +0700

    fix(migrations): ganti menjadi nullOnDelete

--- database/migrations/2026_04_24_023432_create_audit_logs_table.php
+++ database/migrations/2026_04_24_023432_create_audit_logs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('audit_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('user_id')->nullable()->constrained('users')->nullOnDelete();
             $table->string('action');
             $table->text('description')->nullable();
             $table->string('ip_address')->nullable();
