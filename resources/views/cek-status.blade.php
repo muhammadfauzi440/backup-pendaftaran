b62cc3832dda8915a9f3bad1968d3ab290e0e07b
muhammadfauzi440 <muhammadfauzi9953@gmail.com>
2026-05-29 13:05:22 +0700

    style: change font-medium to bold

--- resources/views/cek-status.blade.php
+++ resources/views/cek-status.blade.php
@@ -34,7 +34,7 @@
                         class="flex h-10 w-full rounded-md border border-slate-200 bg-transparent px-3 py-2 text-sm shadow-sm transition-colors placeholder:text-slate-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-red-600 uppercase"
                         placeholder="Contoh: GIN-12345">
                     <button
-                        class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-red-600 disabled:pointer-events-none disabled:opacity-50 bg-red-600 text-white shadow hover:bg-red-600/90 h-10 px-4 py-2 sm:w-auto w-full"
+                        class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-red-600 disabled:pointer-events-none disabled:opacity-50 bg-red-600 text-white shadow hover:bg-red-600/90 h-10 px-4 py-2 sm:w-auto w-full"
                         onclick="cekStatus()">
                         Cek Status
                     </button>
